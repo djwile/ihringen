3e2cf95b3105072e7560f970849589fdcb8188d2
David <87049597+djwile@users.noreply.github.com>
2025-02-19 02:10:13 -0500

    Update search_app.py
    Reflecting current working app

--- search_app/search_app.py
+++ search_app/search_app.py
@@ -4,7 +4,7 @@ import pandas as pd
 import io
 #from app import app
 
-app = Flask(__name__)
+application = Flask(__name__)
 
 CSV_URL = "https://api.github.com/repos/djwile/ihringen/contents/data/ihringen_database-csv.csv?ref=main"
 
@@ -18,16 +18,13 @@ def fetch_csv_data():
     df = pd.read_csv(io.StringIO(csv_data))
     return df
 
-def apply_soundex_filter(df, column, term):
-    soundex_term = soundex(term)
-    return df[df[column].apply(lambda x: soundex(x) == soundex_term)]
 
 def soundex(name):
     name = name.upper()
     soundex = ""
     soundex += name[0]
-    dictionary = {"BFPV": "1", "CGJKQSXZ": "2", "DT": "3", \
-                  "L": "4", "MN": "5", "R": "6", "AEIOUHWY": "."}
+    dictionary = {"BFPV": "1", "CGJKQSXZß": "2", "DT": "3", \
+                  "L": "4", "MN": "5", "R": "6", "AEIOUHWYÄÖÜ": "."}
     for char in name[1:]:
         for key in dictionary.keys():
             if char in key:
@@ -38,25 +35,63 @@ def soundex(name):
     soundex = soundex[:4].ljust(4, "0")
     return soundex
 
-@app.route("/", methods=["GET", "POST"])
+
+def apply_soundex_filter(df, column, term):
+    soundex_term = soundex(term)
+    return df[df[column].apply(lambda x: soundex(x) == soundex_term)]
+
+
+def number_to_string(df):
+    num_cols = df.select_dtypes(include=['number']).columns.tolist()
+    for col in num_cols:
+        if df[col].dtype == 'float64':
+             df[col] = df[col].astype('Int64')
+        else: df
+        df[col] = df[col].astype(str)
+    return df
+
+
+def na_fix(df, dtype, fill):
+    na_cols = df.columns[df.isna().any(axis=0)].union(df.columns[df.eq("<NA>").any(axis=0)])
+    for col in na_cols:
+        df[col] = df[col].fillna(fill).astype(dtype)
+        df[col] = df[col].replace("<NA>", fill)
+    return df
+
+
+@application.route("/", methods=["GET", "POST"])
 def index():
     return render_template("search_app-page.html")
 
-@app.route("/search", methods=["GET", "POST"])
+@application.route("/search", methods=["GET", "POST"])
 def search():
     df = fetch_csv_data()
 #    columns = ["FirstNameNorm", "LastNameNorm", "TownOfOrigin", "Notes", "Year"]
     field_mapping = {
+        "Entry": "EntryNum",
+        "Person ID": "PersonID",
         "Given Name": "FirstNameNorm",
         "Surname": "LastNameNorm",
         "Town": "TownOfOrigin",
         "Comments": "Notes",
         "Year": "Year"
     }
-    filtered_data = df[df["WitnessInd"] == False] if request.form.get("witness-ind") == "on" else df
+    
+    df = number_to_string(df)
+    df = na_fix(df, str, "")
+    
+    #change '0' back to False if using boolean instead of string
+    filtered_data = df[df["WitnessInd"] == '0'] \
+        if request.form.get("witness-ind") == "on" else df
+
+    filtered_data = filtered_data[["EntryNum", "Year", "Date", "Event", "Sex", \
+             "PrimaryInd", "Relationship", "PersonID", "FirstNameNorm", "LastNameNorm", \
+             "TownOfOrigin", "Age", "Status", "Occupation", "Notes", \
+             "BirthXRef", "MarriageXRef", "DeathXRef", "OtherXRef", "OtherXRefEvent", \
+             "Permalink"]] if request.form.get("abridged-data") == "on" else filtered_data
 
     if request.method == "POST":
-        for field in ["Given Name", "Surname", "Town", "Comments", "Year"]:
+        for field in ["Entry", "Person ID", "Given Name", "Surname", "Town", "Comments", "Year"]:
             search_term = request.form.get(f"search-input-{field.replace(' ', '-').lower()}")
             if search_term:
                 column = field_mapping[field]
@@ -80,4 +115,4 @@ def search():
     return render_template("search_app-results.html", columns=filtered_data.columns, data=filtered_data)
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    application.run(debug=True)
