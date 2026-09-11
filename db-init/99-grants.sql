7fbfb5b8536844ec13f54f999a0d56822bb680e9
David <87049597+djwile@users.noreply.github.com>
2026-09-11 14:21:29 -0400

    Add grants for ihringen_appuser

--- db-init/99-grants.sql
+++ db-init/99-grants.sql
@@ -1,6 +1,7 @@
 GRANT CONNECT ON DATABASE ihringen TO ihringen;
 GRANT USAGE ON SCHEMA ihringen TO ihringen;
 GRANT ALL ON ALL TABLES IN SCHEMA ihringen TO ihringen;
+
 GRANT CONNECT ON DATABASE ihringen TO ihringen_appuser;
 GRANT USAGE ON SCHEMA ihringen TO ihringen_appuser;
 GRANT SELECT ON ALL TABLES IN SCHEMA ihringen TO ihringen_appuser;
