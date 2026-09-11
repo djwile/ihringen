599dc0e4e1c05b9ac073e8d8be2424427f0f933a
David <87049597+djwile@users.noreply.github.com>
2026-09-11 14:20:59 -0400

    Add default privileges for ihringen_appuser

--- db-init/99-grants.sql
+++ db-init/99-grants.sql
@@ -4,3 +4,6 @@ GRANT ALL ON ALL TABLES IN SCHEMA ihringen TO ihringen;
 GRANT CONNECT ON DATABASE ihringen TO ihringen_appuser;
 GRANT USAGE ON SCHEMA ihringen TO ihringen_appuser;
 GRANT SELECT ON ALL TABLES IN SCHEMA ihringen TO ihringen_appuser;
+
+ALTER DEFAULT PRIVILEGES IN SCHEMA ihringen
+GRANT SELECT ON TABLES TO ihringen_appuser;
